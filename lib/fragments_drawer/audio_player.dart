9d45f8226c213231968cce6f36bb3a27da75bb1c
andrey.molochko <andrey.molochko@exposit.com>
2019-04-01 08:34:07 -0700

    create navigation drawer

---
+++ lib/fragments_drawer/audio_player.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class CustomAudioPlayer extends StatefulWidget {
+  @override
+  _CustomAudioPlayerState createState() => _CustomAudioPlayerState();
+}
+
+class _CustomAudioPlayerState extends State<CustomAudioPlayer> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
